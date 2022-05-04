ca5313a8f552ee4a4e0ae3556fdbc7ca5cd10c72
Vadim <vadlen2149@gmail.com>
2022-05-04 17:23:45 +0300

    Scroll done

--- src/app/video/video.component.ts
+++ src/app/video/video.component.ts
@@ -8,18 +8,79 @@ import { LinkService } from '../link.service';
   selector: 'app-video',
 })
 export class VideoComponent implements OnInit {
+  public array: [] =  [];
+  public sum = 8;
+  public direction = "";
+  
+  public videoList: Array<any> = [];
 
   public videosInfo = db.videos
 
   constructor(private linkService: LinkService) {
-
+    this.appendItems(0, this.sum);
+    for (let i = 0; i <= 8; i++) {
+      this.loadNewData();
   }
+}
+
+// Полученаем id видео и записываем в linkService
 
  public linkClick($event: any) {
   this.linkService.changeID($event.target.id)
  }
 
-  ngOnInit() {
+// Подгрузка элементов
 
+ngAfterViewInit(): void {
+  document.addEventListener("scroll", () => {
+    this.onScroll();
+  });
+}
+
+public onScroll() {
+  if (this.isCameraTouchesBottom()) {
+    this.loadNewData();
   }
+}
+
+private isCameraTouchesBottom(): boolean {
+  return (document.body.offsetHeight + document.body.offsetTop <= window.scrollY + window.innerHeight + 2);
+}
+
+
+private videosLoaded: number = 0;
+
+public loadNewData(): void {
+  if (this.videosLoaded >= this.videosInfo.length) return;
+
+  this.videoList = [...this.videoList, this.videosInfo[this.videosLoaded]]
+  this.videosLoaded++;
+}
+
+public addItems(startIndex:number, endIndex:number, _method:string) {
+  for (let i = 0; i < this.sum; i++) {
+    //@ts-ignore
+    this.array[_method]([this.videosInfo]);
+  }
+  
+}
+
+public appendItems(startIndex:number, endIndex:number) {
+  this.addItems(startIndex, endIndex, "push");
+}
+
+public onScrollDown() {
+
+  const start = this.sum;
+  this.sum += 4;
+  this.appendItems(start, this.sum);
+
+  this.direction = "down";
+}
+
+
+ ngOnInit(): void {
+ 
+}
+
 }
